bb9de3e42577c84ba13c6e8bede9d1ec2772fe7d
Boris Minin <boris.minin@outlook.com>
2021-07-27 23:44:32 +0300

    Some changes

--- OMSServiceMini/Startup.cs
+++ OMSServiceMini/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using OMSServiceMini.Data;
-
+// in this project i will add pagination in my OMSServiceMini project
 namespace OMSServiceMini
 {
     public class Startup
