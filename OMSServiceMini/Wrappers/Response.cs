3e10cce42bb80b3c6b2fbe14cf582e2031e549b9
Boris Minin <boris.minin@outlook.com>
2021-07-28 16:34:19 +0300

    Add PagedResponse and PaginationFilter for pagination Add Response.cs in folder Wrappers for experiment

---
+++ OMSServiceMini/Wrappers/Response.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OMSServiceMini.Wrappers
+{
+    // show us status, messages or errors
+    public class Response<T>
+    {
+        public Response()
+        {
+        }
+
+        public Response(T data)
+        {
+            Succeeded = true;
+            Message = string.Empty;
+            Errors = null;
+            Data = data;
+        }
+
+        public T Data { get; set; }
+        public bool Succeeded { get; set; }
+        public string[] Errors { get; set; }
+        public string Message { get; set; }
+    }
+}
